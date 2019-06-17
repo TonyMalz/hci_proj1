8c2e38e55038b73afd47e60dc9d2046af8918454
Benedikt Full <benedikt.full@t-online.de>
2019-06-17 10:55:14 +0200

    Move new widget to button at page start

--- src/PageUserview.svelte
+++ src/PageUserview.svelte
@@ -10,8 +10,17 @@
   .userview {
     display: grid;
     height: 100%;
-    grid-template-rows: 50px auto;
+    grid-template-rows: 55px auto;
     grid-gap: 1rem;
+  }
+  .optionsContainer {
+    display: flex;
+    flex-wrap: wrap;
+    overflow: auto;
+  }
+  #newWidgetButton {
+    margin-left: auto;
+    width: 10%;
   }
   .widgetContainer {
     display: flex;
@@ -29,23 +38,36 @@
     padding: 1em;
     border-radius: 4px;
   }
-  .addWidget {
+  /*.addWidget {
     display: grid;
     grid-template-columns: auto 1fr;
     align-items: center;
     cursor: pointer;
     grid-gap: 1em;
-  }
+  }*/
 </style>
 
 <div class="userview" in:fade={{ duration: 300 }}>
-  <select name="user" id="userSelect">
-    <option value="1">User 1 | avg. availability: 6.3 | responses: 223</option>
-    <option value="2">User 2 | avg. availability: 2.3 | responses: 124</option>
-    <option value="3">User 3 | avg. availability: 3.1 | responses: 24</option>
-    <option value="4">User 4 | avg. availability: 4.3 | responses: 424</option>
-    <option value="5">User 5 | avg. availability: 3.3 | responses: 254</option>
-  </select>
+  <div class="optionsContainer">
+    <select name="user" id="userSelect">
+      <option value="1">User 1 | avg. availability: 6.3 | responses: 223</option>
+      <option value="2">User 2 | avg. availability: 2.3 | responses: 124</option>
+      <option value="3">User 3 | avg. availability: 3.1 | responses: 24</option>
+      <option value="4">User 4 | avg. availability: 4.3 | responses: 424</option>
+      <option value="5">User 5 | avg. availability: 3.3 | responses: 254</option>
+    </select>
+    <button id="newWidgetButton">
+      New Widget
+      <svg style="width:25px;height:25px">
+          <path
+            fill="#333"
+            d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59
+            20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0
+            12,22A10,10 0 0,0 22,12A10,10 0 0,0
+            12,2M13,7H11V11H7V13H11V17H13V13H17V11H13V7Z" />
+        </svg>
+    </button>
+  </div>
   <div class="widgetContainer">
     <div class="widget">
       <Anova />
@@ -59,7 +81,7 @@
     <div class="widget">
       <ContextPie />
     </div>
-    <div class="widget">
+    <!--<div class="widget">
       <div class="addWidget">
         <svg style="width:50px;height:50px" viewBox="0 0 24 24">
           <path
@@ -72,6 +94,6 @@
         Add new widget
       </div>
 
-    </div>
+    </div>-->
   </div>
 </div>
